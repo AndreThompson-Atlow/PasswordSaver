db58e66fee687b7f7c262d916945b8122964689b
Jimmy Kudo <kudo@Jimmys-MacBook-Pro.local>
2020-04-28 13:36:18 -0600

    added navbar state and hooks, is now responsive

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -1,55 +1,174 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment, useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+	let location = useLocation();
+	const [activeLinks, setLinks] = useState({
+		dashActive: true,
+		settingsActive: false,
+		welcomeActive: true,
+		loginActive: false,
+		regActive: false,
+	});
+	const offLinks = {
+		dashActive: false,
+		settingsActive: false,
+		welcomeActive: false,
+		loginActive: false,
+		regActive: false,
+	};
+
+	useEffect(() => {
+		switch (location.pathname) {
+			case '/':
+			case '/EasyAccess':
+			case '/SecurePasswords':
+				setLinks({
+					...offLinks,
+					welcomeActive: true,
+				});
+				break;
+			case '/Login':
+				setLinks({
+					...offLinks,
+					loginActive: true,
+				});
+				break;
+			case '/Register':
+				setLinks({
+					...offLinks,
+					regActive: true,
+				});
+				break;
+			case '/Dashboard':
+				setLinks({
+					...offLinks,
+					welcomeActive: true,
+				});
+				break;
+			case '/Settings':
+				setLinks({
+					...offLinks,
+					welcomeActive: true,
+				});
+				break;
+			default:
+				setLinks({
+					...offLinks,
+				});
+		}
+	}, [location.pathname]);
+
+	const {
+		dashActive,
+		settingsActive,
+		welcomeActive,
+		loginActive,
+		regActive,
+	} = activeLinks;
+
+	//If the dashboard is active, it needs to change its style
+	let dashboard = dashActive ? (
+		<Link
+			to='/Dashboard'
+			class='inline-flex items-center px-1 pt-1 border-b-2 border-blue-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-blue-700 transition duration-150 ease-in-out'
+		>
+			Dashboard
+		</Link>
+	) : (
+		<Link
+			to='/Dashboard'
+			class='inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+		>
+			Dashboard
+		</Link>
+	);
+	let settings = settingsActive ? (
+		<Link
+			to='/Settings'
+			class='ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-blue-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-blue-700 transition duration-150 ease-in-out'
+		>
+			Settings
+		</Link>
+	) : (
+		<Link
+			to='/Settings'
+			class='ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+		>
+			Settings
+		</Link>
+	);
+	let welcome = welcomeActive ? (
+		<Link
+			to='/'
+			className='inline-flex items-center px-1 pt-1 border-b-2 border-blue-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-blue-700 transition duration-150 ease-in-out'
+		>
+			Dashboard
+		</Link>
+	) : (
+		<Link
+			to='/'
+			className='inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+		>
+			Dashboard
+		</Link>
+	);
+	let login = loginActive ? (
+		<Link
+			to='/Login'
+			className='ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-blue-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-blue-700 transition duration-150 ease-in-out'
+		>
+			Login
+		</Link>
+	) : (
+		<Link
+			to='/Login'
+			className='ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+		>
+			Login
+		</Link>
+	);
+	let register = regActive ? (
+		<Link
+			to='/Register'
+			className='ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-blue-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-blue-700 transition duration-150 ease-in-out'
+		>
+			Register
+		</Link>
+	) : (
+		<Link
+			to='/Register'
+			className='ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+		>
+			Register
+		</Link>
+	);
+	let logoutTag = (
+		<a
+			onClick={logout}
+			href='#!'
+			class='ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+		>
+			Logout
+		</a>
+	);
+
 	const authLinks = (
 		<div class='text-sm flex-grow'>
-			<Link
-				to='/Dashboard'
-				class='inline-flex items-center px-1 pt-1 border-b-2 border-blue-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-blue-700 transition duration-150 ease-in-out'
-			>
-				Dashboard
-			</Link>
-			<Link
-				to='/Settings'
-				class='ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
-			>
-				Settings
-			</Link>
-
-			<a
-				onClick={logout}
-				href='#!'
-				class='ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
-			>
-				Logout
-			</a>
+			{dashboard}
+			{settings}
+			{logoutTag}
 		</div>
 	);
 
 	const guestLinks = (
 		<div className='text-sm flex-grow'>
-			<Link
-				to='/'
-				className='inline-flex items-center px-1 pt-1 border-b-2 border-blue-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-blue-700 transition duration-150 ease-in-out'
-			>
-				Dashboard
-			</Link>
-			<Link
-				to='/Login'
-				className='ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
-			>
-				Login
-			</Link>
-			<Link
-				to='/Register'
-				className='ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
-			>
-				Register
-			</Link>
+			{welcome}
+			{login}
+			{register}
 		</div>
 	);
 
